a2b0eaaa00891ee8fb845e2641f2fc99c6ac45ae
Ester <egjorek@gmail.com>
2024-08-06 10:32:56 +0200

    First work around the .html modules

---
+++ create_graphs.py
@@ -0,0 +1,114 @@
+import json
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from db import send_graph_to_db, get_from_db, send_to_db
+from datetime import datetime
+
+'''
+ATTENTION! 
+Make sure to run the scraper first, as this requires a json file of the name wta500.json in the active directory, which you get from running the scraper. 
+'''
+#load data
+'''
+#---old way to load data from a single json file 
+with open('wta500.json', 'r', encoding='utf-8') as file:
+    allList = json.load(file)
+
+columns = ['Rank', 'Player', 'Country', 'Points']
+df = pd.DataFrame(allList, columns=columns)
+df['Rank'] = pd.to_numeric(df['Rank'])
+df['Points'] = pd.to_numeric(df['Points'])
+'''
+
+key = 'wtaMasterList' #change the string here to search for different document
+raw = get_from_db(key)
+allList = raw['content']
+columns = ['Rank', 'Player', 'Country', 'Points']
+df = pd.DataFrame(allList, columns=columns)
+df['Rank'] = pd.to_numeric(df['Rank'])
+df['Points'] = pd.to_numeric(df['Points'])
+
+#preparation for grafics
+hm = {}
+for i in df['Country']:
+    if i in hm.keys():
+        hm[i] += 1
+    else:
+        hm[i] = 1
+
+countries_ranked = sorted(hm.items(), key=lambda x:x[1])[::-1]
+send_to_db('countries_ranked', countries_ranked)
+countries = [i[0] for i in countries_ranked]
+appearances = [i[1] for i in countries_ranked]
+
+
+#creating the first grafic
+
+fig, ax = plt.subplots(figsize=(20,10))
+ax.barh(countries[:20], appearances[:20])
+ax.invert_yaxis()
+ax.set_title('countries representation in the WTA rankings top 500',
+             loc='left', )
+ax.xaxis.set_ticks_position('none')
+ax.yaxis.set_ticks_position('none')
+ax.xaxis.set_tick_params(pad=10)
+ax.yaxis.set_tick_params(pad=10)
+
+# saving our work and displaying it
+
+send_graph_to_db('top20_wta500_countries_ranked_by_appearance')
+plt.savefig('top20_wta500_countries_ranked_by_appearance')
+plt.show()
+
+
+# Second Chapter countries representation in Top 100
+hm100 = {}
+for index, country in enumerate(df['Country']):
+    if index < 101:
+        if country in hm100.keys():
+            hm100[country] += 1
+        else:
+            hm100[country] = 1
+
+countries100_ranked = sorted(hm100.items(), key=lambda x:x[1])[::-1]
+send_to_db('countries100_ranked', countries100_ranked)
+countries100 = [i[0] for i in countries100_ranked]
+appearances100 = [i[1] for i in countries100_ranked]
+
+fig100, ax100 = plt.subplots(figsize=(20,10))
+ax100.barh(countries100[:20], appearances100[:20])
+ax100.invert_yaxis()
+ax100.set_title('countries representation in the WTA rankings top 100',
+             loc='left', )
+
+# saving our work and displaying it
+
+send_graph_to_db('top20_wta100_countries_ranked_by_appearance')
+plt.savefig('top20_wta100_countries_ranked_by_appearance')
+plt.show()
+
+#print(countries100, appearances100)
+for i in range(20):
+    print(countries[i], countries100[i])
+    if countries[i] not in countries100[:20]:
+        print(f'this country is in the top 20 when looking at top 500 but not if one only looks at the top 100: {countries[i]}')
+    if countries100[i] not in countries[:20]:
+        print(f'this country is in the top 20 when looking at top 100 but not if one only looks at the top 500: {countries100[i]}')
+
+# comparison bar chart
+comparison = {'top500': [i for i in appearances[:20]],
+              'top100': [i for i in appearances100[:20]]}
+indexprep = []
+for i in range(20):
+    x = str(countries[i]) + ' | ' + str(countries100[i])
+    indexprep.append(x)
+
+comp_df = pd.DataFrame(comparison, columns=['top500', 'top100'], index=indexprep)
+comp_df.plot.barh(figsize=(20, 10), title='comparison of top 20 countries based on ranks in top 500 and top 100 WTA').invert_yaxis()
+
+# saving our work and displaying it
+
+send_graph_to_db('comp_top20_100_500')
+plt.show()
--- scrape_more.py
+++ scrape_more.py
@@ -1,49 +1,95 @@
-import time, json, datetime
-from selenium import webdriver
+from selenium import webdriver  
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from collections import defaultdict
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from page_navigation import open_website, setup_driver, scroll_by_pixels
-from db import send_to_db
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+#/home/a_mind/code/WebScraperSG/tennis_venv/lib/python3.10/site-packages/selenium/webdriver/chrome/__pycache__/webdriver.cpython-310.pyc
+# options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--disable-gpu')
+# driver = webdriver.Chrome(options=options)
 
+# driver.get('https://www.google.com/')
+# print('network access success')
 
-def scrape_data(driver, player_list):
-    elements = driver.find_elements(By.CLASS_NAME, 'hLDjoK')
-    for element in elements:
-        rank = element.find_element(By.CSS_SELECTOR, 'span.Text.gdSPOf').text.rstrip('.')
-        name = element.find_element(By.CSS_SELECTOR, 'bdi.Text.ietnEf').text
-        country = element.find_element(By.CSS_SELECTOR, 'span.Text.erwlKT').text
-        points = element.find_element(By.CSS_SELECTOR, 'span.Text.gYQbnv').text
-        if rank == '' or name == '' or country == '' or points == '':
+def scraping_way_back(link):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://www.google.com/')
+    print('network access success')
+    driver.get(link)
+    print('archive reached')
+    text_box = driver.find_element(by=By.CLASS_NAME, value='js-list-filter-items')
+    stuff = text_box.text.split()
+    print(stuff)
+    data = []
+    temp = text_box.find_elements(By.CLASS_NAME, 'js-list-filter-item')
+
+    for element in temp:
+        try:
+            rank = element.find_element(By.CLASS_NAME, 'cell__content.h4').text
+            name = element.find_element(By.CLASS_NAME, 'cell__content.ff-medium.js-list-filter-value').text
+            country = element.find_element(By.CLASS_NAME, 'cell__content.u-tDim').text
+            points = element.find_element(By.CLASS_NAME, 'cell__section.u-pH12.u-tR.u-w72').text
+            data.append([rank, name, country, points])
+        except Exception as e:
+            print(f'fail {e}')
             continue
-        else:
-            if [rank, name, country, points] not in player_list:
-                player_list.append([rank, name, country, points])
+    driver.close()
+    print(data)           
+    return data
+
+# data = []
+# temp = text_box.find_elements(By.CLASS_NAME, 'js-list-filter-item')
+
+# for element in temp:
+#     try:
+#         rank = element.find_element(By.CLASS_NAME, 'cell__content.h4').text
+#         name = element.find_element(By.CLASS_NAME, 'cell__content.ff-medium.js-list-filter-value').text
+#         country = element.find_element(By.CLASS_NAME, 'cell__content.u-tDim').text
+#         points = element.find_element(By.CLASS_NAME, 'cell__section.u-pH12.u-tR.u-w72').text
+#         data.append([rank, name, country, points])
+#     except Exception as e:
+#         print(f'fail {e}')
+#         continue
+
+# for index, element in enumerate(stuff):
+#     if index % 5 == 0 and index != 0:
+#         temp.append(element)
+#         data.append(temp)
+#         temp = []
+#     else:
+#         temp.append(element)
 
 
-def main(url):
-    driver = setup_driver()
-    open_website(driver, url)
-    player_list = []
+#scraping_way_back('http://web.archive.org/web/20190307025257/https://www.sofascore.com/tennis/rankings/wta')
 
-    # Perform smooth scrolling and scraping
-    end_of_content_reached = False
+def scraping_precise(link, table, subtable, rank_name, name_name, country_name, points_name):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://www.google.com/')
+    print('network access success')
+    driver.get(link)
+    print('archive reached')
+    text_box = driver.find_element(by=By.CLASS_NAME, value=table)
+    stuff = text_box.text.split()
+    print(stuff)
+    data = []
+    temp = text_box.find_elements(By.CLASS_NAME, subtable)
 
-    while not end_of_content_reached:
-        scrape_data(driver, player_list)
-        scroll_by_pixels(driver, 700)
-        if len(player_list) == 500:
-            end_of_content_reached = True
-        print(len(player_list))
-    driver.quit()
-    print(player_list)
-
-    with open('wta500.json', 'w', -1, 'utf-8') as file:
-        json.dump(player_list, file)
-
-    send_to_db('wtaMasterList', player_list)
-
-
-main('https://www.sofascore.com/tennis/rankings/wta')
+    for element in temp:
+        try:
+            rank = element.find_element(By.CLASS_NAME, rank_name).text
+            name = element.find_element(By.CLASS_NAME, name_name).text
+            country = element.find_element(By.CLASS_NAME, country_name).text
+            points = element.find_element(By.CLASS_NAME, points_name).text
+            data.append([rank, name, country, points])
+        except Exception as e:
+            print(f'fail {e}')
+            continue
+    driver.close()
+    print(data)           
+    return data
